8e673ee05d9022a7ebf09c3307696cac52b00fe3
Ilya <milyavskyfamily@gmail.com>
2022-05-17 15:51:37 -0400

    commit tree 7ede0cbe8c01bf6d7c1b23304d63344a0b90eb88

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,19 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  
+  def grades 
+  Student 
 
+  end
+
+  def highest_grade 
+    highest = Student.order("grade DESC").last
+    render json: highest
+  end
+
+  def show 
+  student = Student.find(params[:id])
+  render json: student
+  end
 end
